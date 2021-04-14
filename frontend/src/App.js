cb9d400e5f3a070a18e6f350a020630ab018fe77
Binyam Semret <binyamsemret@Binyams-MacBook-Pro.local>
2021-04-14 12:45:27 -0600

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import './bootstrap.min.css'
 
 const App = () => {
   return (
     <>
-      <h1> Welcome to ethioCommerce </h1>
+      <Header />
+      <main>
+        <Container>
+        <h1> Welcome to ethioCommerce </h1>
+        </Container>
+          
+      </main>
+      <Footer />
     </>
   );
 }
